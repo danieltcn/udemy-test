1004f35999d51b3ca0dcd8d3633bf02c6f3d2ea0
Daniel Turcanu <daniel.turcanu.800@gmail.com>
2021-02-18 12:52:35 +0200

    day 1 in js learning

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,69 @@
 "use strict";
 
-let number = 5;
-const leftBorderWidth = 1;
+let numberOfFilms;
+
+const personalMovieDB = {
+    count: 56894,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+
+//setCountOfMovies();
+//rememberMyFilms();
+//writeYourGeneres();
+//detectPersonalLevel();
+showMyDB(personalMovieDB.privat);
+
+function setCountOfMovies(){
+    
+    
+    numberOfFilms = +prompt('how many movies have you watched?', '');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('how many movies have you watched?', '');
+    }
+}
+
+function rememberMyFilms(){
+    for (let i = 0; i < 2; i++) {
+        const   a = prompt('last film?', ''),
+                b = prompt('rate?', '');
+    
+        if (a != null && b != null && a != '' && b != '' && a.length < 50){
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+        } else {
+            console.log('done');
+            i--;
+        }
+    }
+}
+
+function detectPersonalLevel(){
+    if ( personalMovieDB.count < 10 ){
+        console.log("просмотреномало фильмов");
+    } else if ( personalMovieDB.count < 30 ){
+        console.log("любитель");
+    } else if ( personalMovieDB.count >= 30 ){
+        console.log("киноман");
+    } else {
+        console.log("error");
+    }    
+}
+
+function showMyDB(hidden){
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+function writeYourGeneres(){
+    for(let i = 1; i <= 3; i++){
+        personalMovieDB.genres[i - 1] = prompt(`ваш любимый жанр под номером: ${i}`, '');
+    }
+}
+
+
+
